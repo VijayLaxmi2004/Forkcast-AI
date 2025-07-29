cc3f097dfe6d3c8e9d810f0cb4896d77ce11ddff
VijayLaxmi2004 <vijaylaxmialwaystopper@gmail.com>
2025-07-30 01:39:35 +0530

    Removed config.js from Git and added to .gitignore

--- config.js
+++
@@ -1,9 +0,0 @@
-// config.js - DO NOT COMMIT THIS FILE
-export const firebaseConfig = {
-  apiKey: "AIzaSyDFvTEAc_ERUQDIl7Bo5uwtkQrbm0fvksQ",
-  authDomain: "forkcast-ai-b733d.firebaseapp.com",
-  projectId: "forkcast-ai-b733d",
-  storageBucket: "forkcast-ai-b733d.appspot.com",
-  messagingSenderId: "174542084273",
-  appId: "1:174542084273:web:0270992830c318fc1daa90"
-};
